c8f415a828d8803562073bbc0bdd0d458014745f
Dan.Issler <Dan.Issler@gmail.com>
2017-03-10 09:29:01 -0500

    Cleaned up var names.

--- ExcelHandler/DelegateCommand.vb
+++ ExcelHandler/DelegateCommand.vb
@@ -1,26 +1,26 @@
 ﻿Public Class DelegateCommand
     Implements ICommand
 
-    Private m_canExecute As Func(Of Object, Boolean)
-    Private m_ExecuteAction as Action(Of Object)
-    Private m_canExecuteCache as Boolean
+    Private _canExecute As Func(Of Object, Boolean)
+    Private _executeAction as Action(Of Object)
+    Private _canExecuteCache as Boolean
 
     Public Sub New(ByVal executeAction As Action(Of Object), ByVal canExecute As Func(Of Object, Boolean))
-        Me.m_ExecuteAction = executeAction
-        Me.m_canExecute = canExecute
+        Me._executeAction = executeAction
+        Me._canExecute = canExecute
     End Sub
 
     Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
-        Dim temp As Boolean = m_canExecute(parameter)
-        If m_canExecuteCache <> temp Then
-            m_canExecuteCache = temp
+        Dim temp As Boolean = _canExecute(parameter)
+        If _canExecuteCache <> temp Then
+            _canExecuteCache = temp
             RaiseEvent CanExecuteChanged(Me, New EventArgs())
         End If
-        Return m_canExecuteCache
+        Return _canExecuteCache
     End Function
 
     Public Sub Execute(parameter As Object) Implements ICommand.Execute
-        m_ExecuteAction(parameter)
+        _executeAction(parameter)
     End Sub
 
     Public Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
